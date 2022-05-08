3c8a0db15563e80f43a9a0be04c573257712bda5
SAGUINIZINHO VK <92821616+saguinizinho@users.noreply.github.com>
2022-05-08 11:02:09 -0300

    Create exemplo de recursão.cpp

---
+++ exemplo de recursão.cpp
@@ -0,0 +1,21 @@
+#incluir<iostream>
+usando namespace std;
+
+diversão vazia (int x)
+{
+	se (x==0)
+	{
+		retornar;
+	}
+	mais{
+ cout<< "Geeks"<<" ";
+		diversão (x-1);
+ cout<<"para geeks"<<" ";
+	}
+	
+}
+int principal()
+{
+	diversão(3);
+	retorno 0;
+}
